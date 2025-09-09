0061f9f1fabb76fe4756a6a0f8f01d53c35e4e56
Jeba18 <jonahjeabrson@gmail.com>
2025-09-09 13:11:06 +0530

    Replace old code with new project

---
+++ backend/src/main/java/com/wellnessxpert/WellnessXpertApplication.java
@@ -0,0 +1,13 @@
+package com.wellnessxpert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WellnessXpertApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WellnessXpertApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/wellnessxpert/WellnessXpertApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wellnessxpert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WellnessXpertApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
